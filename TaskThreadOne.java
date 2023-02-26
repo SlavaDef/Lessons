b5754c23c595f2cc5eecb364f498e79f79afec17
SlavaDef <115735844+SlavaDef@users.noreply.github.com>
2023-02-26 14:41:36 +0300

    Add files via upload

--- TaskThreadOne.java
+++ TaskThreadOne.java
@@ -1,17 +1,14 @@
 package HomeWork12;
 
-// Напишіть програму, яка кожну секунду відображає на екрані дані про час, що минув від моменту запуску програми.
-//
-//Другий потік цієї ж програми кожні 5 секунд виводить повідомлення Минуло 5 секунд.
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class TaskThreadOne {
-
     private static volatile boolean running = true;
-
     private static long before = System.currentTimeMillis();
 
     public static  void main(String[] args) {
-
         new Thread(() -> {
 
             while (running) {
@@ -28,18 +25,12 @@ public class TaskThreadOne {
 
         }).start();
 
-        new Thread(() -> {
-
-            while (running) {
-
-                try {
-                    Thread.sleep(5000);
-                    System.out.println("5 seconds have passed! ");
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-
-        }).start();
+    ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
+     scheduledExecutorService.scheduleAtFixedRate(
+             () -> System.out.println("5 seconds have passed!"),
+             5,
+             5,
+    TimeUnit.SECONDS
+           );
     }
 }
