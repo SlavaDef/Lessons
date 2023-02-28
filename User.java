29dd8c69275693d7a826cff8f903da7bc0076785
SlavaDef <115735844+SlavaDef@users.noreply.github.com>
2023-02-28 19:53:34 +0300

    Add files via upload

--- User.java
+++ User.java
@@ -5,21 +5,23 @@ import java.util.Objects;
 
 public class User {
 
-    private int id;
-    private  String name;
+    private String id;
+    private String name;
     private String username;
     private String email;
-    private String [] adress = { "street", "suite", "city", "zipcode" };
-    private String [] geo = { "lat", "lng" };
+
+    private Address address;
+
     private String phone;
     private String website;
-    private String [] company = {"name", "catchPhrase", "bs" };
 
-    public int getId() {
+    private Company company;
+
+    public String getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(String id) {
         this.id = id;
     }
 
@@ -47,20 +49,20 @@ public class User {
         this.email = email;
     }
 
-    public String[] getAdress() {
-        return adress;
+    public Address getAddress() {
+        return address;
     }
 
-    public void setAdress(String[] adress) {
-        this.adress = adress;
+    public void setAddress(Address address) {
+        this.address = address;
     }
 
-    public String[] getGeo() {
-        return geo;
+    public Company getCompany() {
+        return company;
     }
 
-    public void setGeo(String[] geo) {
-        this.geo = geo;
+    public void setCompany(Company company) {
+        this.company = company;
     }
 
     public String getPhone() {
@@ -79,72 +81,102 @@ public class User {
         this.website = website;
     }
 
-    public String[] getCompany() {
-        return company;
-    }
-
-    public void setCompany(String[] company) {
-        this.company = company;
-    }
-
-    public User(int id, String name, String username, String email) {
-        this.id = id;
-        this.name = name;
-        this.username = username;
-        this.email = email;
-    }
-
-    public User() {
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User abUser = (User) o;
-        return id == abUser.id && Objects.equals(name, abUser.name) && Objects.equals(username, abUser.username) && Objects.equals(email, abUser.email) && Arrays.equals(adress, abUser.adress) && Arrays.equals(geo, abUser.geo) && Objects.equals(phone, abUser.phone) && Objects.equals(website, abUser.website) && Arrays.equals(company, abUser.company);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = Objects.hash(id, name, username, email, phone, website);
-        result = 31 * result + Arrays.hashCode(adress);
-        result = 31 * result + Arrays.hashCode(geo);
-        result = 31 * result + Arrays.hashCode(company);
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", username='" + username + '\'' +
-                ", email='" + email + '\'' +
-                '}';
-    }
-
-    /*
-
-    "id": 1,
-    "name": "Leanne Graham",
-    "username": "Bret",
-    "email": "Sincere@april.biz",
-    "address": {
-      "street": "Kulas Light",
-      "suite": "Apt. 556",
-      "city": "Gwenborough",
-      "zipcode": "92998-3874",
-      "geo": {
-        "lat": "-37.3159",
-        "lng": "81.1496"
-      }
-    },
-    "phone": "1-770-736-8031 x56442",
-    "website": "hildegard.org",
-    "company": {
-      "name": "Romaguera-Crona",
-      "catchPhrase": "Multi-layered client-server neural-net",
-      "bs": "harness real-time e-markets"
-     */
 }
+    class Address {
+        private String street;
+        private String suite;
+        private String city;
+        private String zipcode;
+        private Geo geo;
+
+        public String getStreet() {
+            return street;
+        }
+
+        public void setStreet(String street) {
+            this.street = street;
+        }
+
+        public String getSuite() {
+            return suite;
+        }
+
+        public void setSuite(String suite) {
+            this.suite = suite;
+        }
+
+        public String getCity() {
+            return city;
+        }
+
+        public void setCity(String city) {
+            this.city = city;
+        }
+
+        public String getZipcode() {
+            return zipcode;
+        }
+
+        public void setZipcode(String zipcode) {
+            this.zipcode = zipcode;
+        }
+
+        public Geo getGeo() {
+            return geo;
+        }
+
+        public void setGeo(Geo geo) {
+            this.geo = geo;
+        }
+    }
+
+    class Company {
+        private String name;
+        private String catchPhrase;
+        private String bs;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getCatchPhrase() {
+            return catchPhrase;
+        }
+
+        public void setCatchPhrase(String catchPhrase) {
+            this.catchPhrase = catchPhrase;
+        }
+
+        public String getBs() {
+            return bs;
+        }
+
+        public void setBs(String bs) {
+            this.bs = bs;
+        }
+    }
+
+    class Geo {
+        private double lat;
+        private double lng;
+
+        public double getLat() {
+            return lat;
+        }
+
+        public void setLat(double lat) {
+            this.lat = lat;
+        }
+
+        public double getLng() {
+            return lng;
+        }
+
+        public void setLng(double lng) {
+            this.lng = lng;
+        }
+    }
